9918eb8e024756a9cf71be55e3eed669acd7756d
luvemil <taran.marco@gmail.com>
2016-11-27 00:35:57 +0100

    Mount Navbar instead of header

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
-// import Navbar from './Navbar';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Navbar />
         {this.props.children}
         <Footer />
       </div>
---
+++ app/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <nav className='navbar navbar-default'>
+        <div className='container-fluid'>
+          <div className='navbar-header'>
+            <a className='navbar-brand'>
+              SyncPlayer
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
